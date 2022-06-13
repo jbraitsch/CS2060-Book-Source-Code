ce14bb82157b19c7bc5249718896b24ba74d5100
debmhteach <debmh.teach@gmail.com>
2022-06-13 12:50:28 -0600

    Update fig03_06.c

--- examples/ch03/fig03_06.c
+++ examples/ch03/fig03_06.c
@@ -1,45 +1,37 @@
 // Fig. 3.6: fig03_06.c
 // Class average program with counter-controlled iteration
+
 #include <stdio.h>
 
-// function main begins program execution
+//const to hold number of grades so not hard coded
+const unsigned int NUMBER_GRADES = 10;
+
+
 int main( void )
 {
-   unsigned int counter; // number of grade to be entered next
-   int grade; // grade value
-   int total; // sum of grades entered by user
-   int average; // average of grades
+   //initialize variables 
+   unsigned int counter = 0; // number of grade entered
+   int grade =0 ; // grade value
+   int total =0; // sum of grades entered by user
+   double average =0; // average of grades
    
-   // initialization phase
-   total = 0; // initialize total
-   counter = 1; // initialize loop counter
-   
-   // processing phase
-   while ( counter <= 10 ) { // loop 10 times
-      printf( "%s", "Enter grade: " ); // prompt for input
-      scanf( "%d", &grade ); // read grade from user
-      total = total + grade; // add grade to total
-      counter = counter + 1; // increment counter
+  printf("Enter %lu grades.\n", NUMBER_GRADES);
+  
+   //enter and sum grades
+   while ( counter < NUMBER_GRADES) {
+       counter = counter + 1;
+      printf("Enter grade %d: ",  counter); 
+      scanf( "%d", &grade ); 
+      total = total + grade; 
+      
    } // end while
    
-   // termination phase
-   average = total / 10; // integer division
+   //average grades but need to cast to float to get acurate average
+   average = (double)total / NUMBER_GRADES; // integer division
 
-   printf( "Class average is %d\n", average ); // display result
+   printf( "Class average is %lf\n", average ); // display result
+
+
+   return 0; //return value of main function
 } // end function main
-
-/**************************************************************************
- * (C) Copyright 1992-2013 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
 
