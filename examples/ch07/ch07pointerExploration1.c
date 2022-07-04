c5ebaced42f70fece90408e5ee5dd59c156a3efd
jbraitsch <joe.braitsch3@gmail.com>
2022-07-04 15:06:52 -0600

    Pointers
    Look at how pointer can be used

--- examples/ch07/ch07pointerExploration1.c
+++ examples/ch07/ch07pointerExploration1.c
@@ -15,24 +15,33 @@ int main ()
 	int calcHouseNum2 = 0;
 	int *houseNumPtr = &houseNum;
 	
+	//prints value of houseNum
 	printf("houseNum %d\n", houseNum);
 
+	//prints the address of houseNum
 	printf("&houseNum %p\n\n", &houseNum);
 
+	//prints value of houseNum
 	printf ("*houseNumPtr  %d\n", *houseNumPtr);
 
+	//prints address held in pointer (&houseNum)
 	printf ("houseNumPtr %p\n\n", houseNumPtr);
 
+	//prints address of the pointer
 	printf ("&houseNumPtr %p\n\n", &houseNumPtr);
 
+	//adds houseNum + houseNum
 	calcHouseNum1 =  *houseNumPtr + *houseNumPtr;
 
 	printf("*houseNumPtr + *houseNumPtr is %d\n\n", calcHouseNum1);
 
+	//multiplies houseNum by 2 equal to calcHouseNum1
 	calcHouseNum2 = 2 * (*houseNumPtr);
 
 	printf("2 * (*houseNumPtr) is %d\n\n", calcHouseNum2);
 
+	//prints address of where the value that ptr is pointing at (houseNum)
+	//both get the same address just in a different order
 	printf("\n&*houseNumPtr = %p"
 		"\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr);
 			
--- examples/ch07/ch07sec7_4CubePassRef.c
+++ examples/ch07/ch07sec7_4CubePassRef.c
@@ -14,17 +14,19 @@ int main(void) {
 	
 	printf ("In main before cubePass1  numberMain = %d\n", numberMain);
 	printf("&numberMain = %p\n", &numberMain);
-	result = cubePass1(numberMain);
+	result = cubePass1(numberMain);//doesn't change value of numberMain in function
 	printf ("In main after cubePass1  numberMain = %d\n", numberMain);
 	printf ("Result = %d\n", result);
 	printf("\nIn main before cubePass2  numberMain = %d\n", numberMain);
-	result = cubePass2(&numberMain);
+	result = cubePass2(&numberMain);//changes value of numberMain in function
 	printf("\nIn main after cubePass2  numberMain = %d\n", numberMain);
 	printf("result = %d\n", result);
 
 } // main
 
 
+//pass by value
+//returns value of cube and stored in result in main
 int cubePass1 (int number) 
 {
 	int cube = 0;
@@ -38,14 +40,27 @@ int cubePass1 (int number)
 	return cube;
 } 
 
+//pass by reference
+//numberMain is changed while function is running
+//result is changed in main when cube is returned
 int cubePass2 (int * numberPtr) 
 {
 	int cube = 0;
 	puts ("\nIn cubePass2");
+	
+	//prints the address stored in the pointer
 	printf("numberPtr = %p\n", numberPtr);
+
+	//prints value in pointed variable
 	printf ("*numberPtr = %d\n", *numberPtr);
+
+	//prints address of the pointer
 	printf("&numberPtr = %p\n", &numberPtr);
+
+	//cubes the pointed at variable
 	cube = (* numberPtr )* (* numberPtr )* (* numberPtr); 
+
+	//assigns value of cube to pointed variable (numberMain)
 	*numberPtr = cube;
 	printf ("*numberPtr = %d\n", *numberPtr);
 	return cube;
